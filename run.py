7b64c331dec2c7b82bcb3930dfb234fe4273f28f
Print-Null <xiaofengyang@wavely.com>
2022-10-27 18:36:18 +0800

    allure report dir

--- run.py
+++ run.py
@@ -2,15 +2,16 @@
 import pytest
 import os
 import sys
-import test
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
-    allure_results = test.allure_results_path
-    if not os.path.exists(allure_results):
-        os.makedirs(allure_results)
-    allure_reports = test.allure_report_path
-    if not os.path.exists(allure_reports):
-        os.makedirs(allure_reports)
-    pytest.main(["-s", "test.py", "--alluredir", allure_results])
-    os.system(f"allure generate {allure_results} -o {allure_reports}")
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    base_dir = base_dir.replace("\\", "/") + "/"
+    allure_results_path = os.path.join(base_dir, "allure-results")
+    allure_report_path = os.path.join(base_dir, 'allure-results/reports')
+    if not os.path.exists(allure_results_path):
+        os.makedirs(allure_results_path)
+    if not os.path.exists(allure_report_path):
+        os.makedirs(allure_report_path)
+    pytest.main(["-s", "test.py", "--alluredir", allure_results_path])
+    os.system(f"allure generate {allure_results_path} -o {allure_report_path}")
--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
 import os
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-base_dir = base_dir.replace("\\", "/") + "/"
-allure_results_path = os.path.join(base_dir, "allure-results")
-allure_report_path = os.path.join(base_dir, 'allure-results/reports')
 # chromedriver = base_dir + "tools/chrome/chromedriver"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
