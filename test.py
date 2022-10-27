566b1b4df6c8d5c821d8379306af844bbb6258a3
Print-Null <57884129+Print-Null@users.noreply.github.com>
2022-10-27 17:18:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,13 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 base_dir = base_dir.replace("\\", "/") + "/"
 allure_results_path = os.path.join(base_dir, "allure-results")
 allure_report_path = os.path.join(base_dir, 'allure-results/reports')
-chromedriver = base_dir + "tools/chrome/chromedriver"
+# chromedriver = base_dir + "tools/chrome/chromedriver"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-print(chromedriver)
-# chromedriver = "/usr/bin/chromedriver"
+chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
 
