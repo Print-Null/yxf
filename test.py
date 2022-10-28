3c1375016e6a7654df1d3bc0007c05dfe488fccc
Print-Null <xiaofengyang@wavely.com>
2022-10-28 10:49:28 +0800

    chromedriver

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 import os
-
+from selenium.webdriver.chrome.service import Service
 # chromedriver = base_dir + "tools/chrome/chromedriver"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
+# chromedriver = "/usr/bin/chromedriver"
+# s = Service(r"D:\chromedriver\106.0.5249.61\chromedriver.exe")
+
+# os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(options=chrome_options)
 
 
 def test_baidu():
